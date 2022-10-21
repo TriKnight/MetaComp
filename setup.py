e5b65c9ea82dec554def2d5a4275961b4d7b3b7b
TriKnight <nvn.knightdragon@gmail.com>
2022-10-21 14:39:23 +0700

    add all files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(     
+    name='MetaComp',
+    author='TriKnight',     
+    version='0.1',     
+    install_requires=[         
+        'pandas',         
+        'numpy',
+        'matplotlib',
+        'scikit'
+    ],
+    # ... more options/metadata
+)
